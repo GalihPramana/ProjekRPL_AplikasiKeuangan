3c2bc15cb03b0814edcf1cfd3247de518bd59060
Jamesmarvins <james.marvin@ti.ukdw.ac.id>
2025-05-08 20:45:34 +0700

    Update

--- Homepage/src/main/java/UtangPiutang/UtangPiutangController.java
+++ Homepage/src/main/java/UtangPiutang/UtangPiutangController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import model.UtangData;
 
 public class UtangPiutangController {
 
